ee4cba860679e66e9190aae34d85d795dfb03827
Rodrigo Nairnei <ronairnei@gmail.com>
2019-06-14 19:37:36 -0300

    Update TwitterUtils.java
    Adicionado comentário com link para geração dos tokens.

--- app/src/main/java/com/example/rodrigonairnei/tweet/TwitterUtils.java
+++ app/src/main/java/com/example/rodrigonairnei/tweet/TwitterUtils.java
@@ -12,11 +12,12 @@ import twitter4j.auth.AccessToken;
 
 public class TwitterUtils {
 
-     //Editáveis por aplicativo
-     private static final String token = "4853844742-1c9rMropdDE7cH6KDRLQ2VhFKgCfndoDY9euBbA";
-     private static final String tokenSecret = "Zp6OhNkavVZXXL0a3XbVv97K0sKjkrbMxeKlXMM6Z9ru8";
-     private static final String consumerKey = "jKJIfgA2q1jqzBI3NE3XrrhSI";
-     private static final String consumerSecret = "kErjGpyKY8bP63w81szl3TVXFJIHQ07JZL8M3dHp3BqwI1Mtow";
+     //https://developer.twitter.com/en/docs/basics/authentication/guides/securing-keys-and-tokens.html
+     
+     private static final String token = ""; //TODO
+     private static final String tokenSecret = ""; // TODO
+     private static final String consumerKey = ""; //TODO
+     private static final String consumerSecret = ""; //TODO
 
      //Site para redirecinamento de tweets
      private static final String tweetLink = "https://twitter.com/tweet/status/";
